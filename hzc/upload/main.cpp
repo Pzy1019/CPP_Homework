c0facf1fb17ed5aad1f825d464a77e4dca5da6d6
hohq <1984570111@qq.com>
2023-06-02 12:16:00 +0800

    HZC's pictureadjusting part(unfinished)

---
+++ hzc/upload/main.cpp
@@ -0,0 +1,19 @@
+﻿#include "picture.h"
+#include "Header.h"
+#include <QtWidgets/QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    picture w;
+    w.show();
+    connect(w.btn2, SIGNAL(sigClicked()), this, SLOT(close)));
+    return a.exec();
+
+    /*Mat scr = imread("D:/lovely pic/6489669122.jpg");
+    namedWindow("img", 0);
+    imshow("img", scr);
+    waitKey(0);
+    destroyAllWindows();
+    return 0;*/
+}
